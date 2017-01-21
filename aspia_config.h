f077000552c59da142bbed0bc7bc1820c77b08c5
Dmitry Chapyshev <dmitry@aspia.ru>
2017-01-21 14:33:40 +0500

    Sync with my local repository (part 9/9)

--- aspia_config.h
+++ aspia_config.h
@@ -39,7 +39,7 @@
 #define WINVER           0x0501
 #define _WIN32_WINNT     0x0501
 #define _WIN32_WINDOWS   0x0501
-#define NTDDI_VERSION    0x05010300 // Windows XP SP3
+#define NTDDI_VERSION    0x05010000 // Windows XP
 #define _WIN32_IE        0x0800 // Internet Explorer 8.0
 #define PSAPI_VERSION    1
 
