a507e13a6dbe1dcdbb370e1a6d5ed338391997ce
Dmitry Chapyshev <dmitry@aspia.ru>
2017-05-11 03:02:19 +0500

    - Add scoped_comptr and scoped_refptr for replacing WRL Step 2/x to return support for Windows XP

---
+++ base/scoped_refptr.h
@@ -0,0 +1,286 @@
+//
+// PROJECT:         Aspia Remote Desktop
+// FILE:            base/scoped_refptr.h
+// LICENSE:         See top-level directory
+// PROGRAMMERS:     Dmitry Chapyshev (dmitry@aspia.ru)
+//
+
+#ifndef _ASPIA_BASE__SCOPED_REFPTR_H
+#define _ASPIA_BASE__SCOPED_REFPTR_H
+
+namespace aspia {
+
+//
+// A smart pointer class for reference counted objects.  Use this class instead
+// of calling AddRef and Release manually on a reference counted object to
+// avoid common memory leaks caused by forgetting to Release an object
+// reference.  Sample usage:
+//
+//   class MyFoo : public RefCounted<MyFoo> {
+//    ...
+//    private:
+//     friend class RefCounted<MyFoo>;  // Allow destruction by RefCounted<>.
+//     ~MyFoo();                        // Destructor must be private/protected.
+//   };
+//
+//   void some_function() {
+//     scoped_refptr<MyFoo> foo = new MyFoo();
+//     foo->Method(param);
+//     // |foo| is released when this function returns
+//   }
+//
+//   void some_other_function() {
+//     scoped_refptr<MyFoo> foo = new MyFoo();
+//     ...
+//     foo = nullptr;  // explicitly releases |foo|
+//     ...
+//     if (foo)
+//       foo->Method(param);
+//   }
+//
+// The above examples show how scoped_refptr<T> acts like a pointer to T.
+// Given two scoped_refptr<T> classes, it is also possible to exchange
+// references between the two objects, like so:
+//
+//   {
+//     scoped_refptr<MyFoo> a = new MyFoo();
+//     scoped_refptr<MyFoo> b;
+//
+//     b.swap(a);
+//     // now, |b| references the MyFoo object, and |a| references nullptr.
+//   }
+//
+// To make both |a| and |b| in the above example reference the same MyFoo
+// object, simply use the assignment operator:
+//
+//   {
+//     scoped_refptr<MyFoo> a = new MyFoo();
+//     scoped_refptr<MyFoo> b;
+//
+//     b = a;
+//     // now, |a| and |b| each own a reference to the same MyFoo object.
+//   }
+//
+template <class T>
+class scoped_refptr
+{
+public:
+    typedef T element_type;
+
+    scoped_refptr() = default;
+
+    scoped_refptr(T* p) : ptr_(p)
+    {
+        if (ptr_)
+            AddRef(ptr_);
+    }
+
+    // Copy constructor.
+    scoped_refptr(const scoped_refptr<T>& r) : ptr_(r.ptr_)
+    {
+        if (ptr_)
+            AddRef(ptr_);
+    }
+
+    // Copy conversion constructor.
+    template <typename U, typename = typename std::enable_if<std::is_convertible<U*, T*>::value>::type>
+    scoped_refptr(const scoped_refptr<U>& r) : ptr_(r.get())
+    {
+        if (ptr_)
+            AddRef(ptr_);
+    }
+
+    // Move constructor. This is required in addition to the conversion
+    // constructor below in order for clang to warn about pessimizing moves.
+    scoped_refptr(scoped_refptr&& r) : ptr_(r.get()) { r.ptr_ = nullptr; }
+
+    // Move conversion constructor.
+    template <typename U, typename = typename std::enable_if<std::is_convertible<U*, T*>::value>::type>
+    scoped_refptr(scoped_refptr<U>&& r) : ptr_(r.get())
+    {
+        r.ptr_ = nullptr;
+    }
+
+    ~scoped_refptr()
+    {
+        if (ptr_)
+            Release(ptr_);
+    }
+
+    T* get() const { return ptr_; }
+
+    T& operator*() const
+    {
+        assert(ptr_ != nullptr);
+        return *ptr_;
+    }
+
+    T* operator->() const
+    {
+        assert(ptr_ != nullptr);
+        return ptr_;
+    }
+
+    scoped_refptr<T>& operator=(T* p)
+    {
+        // AddRef first so that self assignment should work
+        if (p)
+            AddRef(p);
+        T* old_ptr = ptr_;
+        ptr_ = p;
+        if (old_ptr)
+            Release(old_ptr);
+        return *this;
+    }
+
+    scoped_refptr<T>& operator=(const scoped_refptr<T>& r)
+    {
+        return *this = r.ptr_;
+    }
+
+    template <typename U>
+    scoped_refptr<T>& operator=(const scoped_refptr<U>& r)
+    {
+        return *this = r.get();
+    }
+
+    scoped_refptr<T>& operator=(scoped_refptr<T>&& r)
+    {
+        scoped_refptr<T>(std::move(r)).swap(*this);
+        return *this;
+    }
+
+    template <typename U>
+    scoped_refptr<T>& operator=(scoped_refptr<U>&& r)
+    {
+        scoped_refptr<T>(std::move(r)).swap(*this);
+        return *this;
+    }
+
+    void swap(T** pp)
+    {
+        T* p = ptr_;
+        ptr_ = *pp;
+        *pp = p;
+    }
+
+    void swap(scoped_refptr<T>& r)
+    {
+        swap(&r.ptr_);
+    }
+
+    explicit operator bool() const { return ptr_ != nullptr; }
+
+    template <typename U>
+    bool operator==(const scoped_refptr<U>& rhs) const
+    {
+        return ptr_ == rhs.get();
+    }
+
+    template <typename U>
+    bool operator!=(const scoped_refptr<U>& rhs) const
+    {
+        return !operator==(rhs);
+    }
+
+    template <typename U>
+    bool operator<(const scoped_refptr<U>& rhs) const
+    {
+        return ptr_ < rhs.get();
+    }
+
+protected:
+    T* ptr_ = nullptr;
+
+private:
+    // Friend required for move constructors that set r.ptr_ to null.
+    template <typename U>
+    friend class scoped_refptr;
+
+    // Non-inline helpers to allow:
+    //     class Opaque;
+    //     extern template class scoped_refptr<Opaque>;
+    // Otherwise the compiler will complain that Opaque is an incomplete type.
+    static void AddRef(T* ptr);
+    static void Release(T* ptr);
+};
+
+// static
+template <typename T>
+void scoped_refptr<T>::AddRef(T* ptr)
+{
+    ptr->AddRef();
+}
+
+// static
+template <typename T>
+void scoped_refptr<T>::Release(T* ptr)
+{
+    ptr->Release();
+}
+
+// Handy utility for creating a scoped_refptr<T> out of a T* explicitly without
+// having to retype all the template arguments
+template <typename T>
+scoped_refptr<T> make_scoped_refptr(T* t)
+{
+    return scoped_refptr<T>(t);
+}
+
+template <typename T, typename U>
+bool operator==(const scoped_refptr<T>& lhs, const U* rhs)
+{
+    return lhs.get() == rhs;
+}
+
+template <typename T, typename U>
+bool operator==(const T* lhs, const scoped_refptr<U>& rhs)
+{
+    return lhs == rhs.get();
+}
+
+template <typename T>
+bool operator==(const scoped_refptr<T>& lhs, std::nullptr_t null)
+{
+    return !static_cast<bool>(lhs);
+}
+
+template <typename T>
+bool operator==(std::nullptr_t null, const scoped_refptr<T>& rhs)
+{
+    return !static_cast<bool>(rhs);
+}
+
+template <typename T, typename U>
+bool operator!=(const scoped_refptr<T>& lhs, const U* rhs)
+{
+    return !operator==(lhs, rhs);
+}
+
+template <typename T, typename U>
+bool operator!=(const T* lhs, const scoped_refptr<U>& rhs)
+{
+    return !operator==(lhs, rhs);
+}
+
+template <typename T>
+bool operator!=(const scoped_refptr<T>& lhs, std::nullptr_t null)
+{
+    return !operator==(lhs, null);
+}
+
+template <typename T>
+bool operator!=(std::nullptr_t null, const scoped_refptr<T>& rhs)
+{
+    return !operator==(null, rhs);
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream& out, const scoped_refptr<T>& p)
+{
+    return out << p.get();
+}
+
+} // namespace aspia
+
+#endif // _ASPIA_BASE__SCOPED_REFPTR_H
