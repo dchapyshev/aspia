4db057d914541b53e759ec609d6a16ebbb495485
Dmitry Chapyshev <dmitry@aspia.ru>
2017-05-22 15:46:15 +0500

    - Started support for x64 building (not finished yet)

--- build_config.h
+++ build_config.h
@@ -8,6 +8,28 @@
 #ifndef _ASPIA_BUILD_CONFIG_H
 #define _ASPIA_BUILD_CONFIG_H
 
+// Compiler detection.
+#if defined(_MSC_VER)
+#define COMPILER_MSVC 1
+#else
+#error Please add support for your compiler in build_config.h
+#endif
+
+// Processor architecture detection.
+#if defined(_M_X64) || defined(__x86_64__)
+#define ARCH_CPU_X86_FAMILY      1
+#define ARCH_CPU_X86_64          1
+#define ARCH_CPU_64_BITS         1
+#define ARCH_CPU_LITTLE_ENDIAN   1
+#elif defined(_M_IX86) || defined(__i386__)
+#define ARCH_CPU_X86_FAMILY      1
+#define ARCH_CPU_X86             1
+#define ARCH_CPU_32_BITS         1
+#define ARCH_CPU_LITTLE_ENDIAN   1
+#else
+#error Please add support for your processor architecture in build_config.h
+#endif
+
 // Target version
 #define _WIN32_WINNT     0x0501
 #define NTDDI_VERSION    0x05010300 // Windows XP SP3
