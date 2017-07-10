e26b3d703f711385903eca3266f13172d8843804
Dmitry Chapyshev <dmitry@aspia.ru>
2017-07-11 02:12:46 +0500

    - clean.cmd: delete all *.obj, *.pdb, *.res, *.exe files at cleaning - Ignore *.obj, *.pdb, *.res, *.exe files in Git

--- clean.cmd
+++ clean.cmd
@@ -1,6 +1,10 @@
 pushd %~dp0
-del   /S /Q *.user
 rmdir /S /Q Debug
 rmdir /S /Q Release
 rmdir /S /Q .vs
+del   /S /Q *.user
+del   /S /Q *.obj
+del   /S /Q *.pdb
+del   /S /Q *.res
+del   /S /Q *.exe
 popd
