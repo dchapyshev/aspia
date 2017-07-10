6982a23fb1eeacf3f6d5cae9269776effaa94fe8
Dmitry Chapyshev <dmitry@aspia.ru>
2017-07-11 01:38:32 +0500

    - Running unit tests for the libsodium after compilation. If at least one test fails, the compilation fails with an error

--- 3rdparty/libsodium/test/default/wintest.bat
+++ 3rdparty/libsodium/test/default/wintest.bat
@@ -1,37 +1,27 @@
 @ECHO OFF
 
 if "%1" == "" (
-  echo "Usage: wintest.bat <Release | ReleaseDLL | Debug | DebugDLL"
-	goto :END
+  echo "Usage: wintest.bat <Release | Debug"
+	goto :ERROR
 )
 
 if not exist sodium_version.c (
 	CD test\default
 	if not exist sodium_version.c (
 		echo "Are you on the right path?" %CD%
-		goto :END
+		goto :ERROR
 	)
 )
 
 if "%2" == "x64" (SET ARCH=x64) else (SET ARCH=Win32)
 SET CFLAGS=/nologo /DTEST_SRCDIR=\".\" /I..\..\src\libsodium\include\sodium /I..\..\src\libsodium\include /I..\quirks
-SET LDFLAGS=/link /LTCG advapi32.lib ..\..\Build\%1\%ARCH%\libsodium.lib
-if "%1" == "ReleaseDLL" ( goto :ReleaseDLL )
-if "%1" == "DebugDLL"   ( goto :DebugDLL )
+SET LDFLAGS=/link /LTCG advapi32.lib ..\..\..\..\%1\%ARCH%\libsodium.lib
 if "%1" == "Release"   ( goto :Release )
 if "%1" == "Debug"   ( goto :Debug )
 echo "Invalid build type"
-goto :END
-:ReleaseDLL
-	SET CFLAGS=%CFLAGS% /MD /Ox 
-	SET PATH=..\..\Build\%1\%ARCH%;%PATH% 
-	goto :COMPILE
+goto :ERROR
 :Release
 	SET CFLAGS=%CFLAGS% /MT /Ox /DSODIUM_STATIC /DSODIUM_EXPORT=
-	goto :COMPILE
-:DebugDLL
-	SET CFLAGS=%CFLAGS% /GS /MDd /Od
-	SET PATH=..\..\Build\%1\%ARCH%;%PATH%
 	goto :COMPILE
 :Debug
 	SET CFLAGS=%CFLAGS% /GS /MTd /Od /DSODIUM_STATIC /DSODIUM_EXPORT=
@@ -42,15 +32,18 @@ FOR %%f in (*.c) DO (
 	cl %CFLAGS% %%f %LDFLAGS% /OUT:%%f.exe > NUL 2>&1
 	if not exist %%f.exe (
 		echo %%f compile failed
-		goto :END
+		goto :ERROR
 	)
 	%%f.exe
 	if errorlevel 1 ( 
 		echo %%f failed
+		goto :ERROR
 	) else (
 		echo %%f ok
 	)
 )
 REM Remove temporary files
 del *.exe *.obj *.res 
-:END
+EXIT /B 0
+:ERROR
+EXIT /B 1
