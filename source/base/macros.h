5b67afb31baa877af7b56ba4e6334e713ee526ab
Dmitry Chapyshev <dmitry@aspia.ru>
2017-07-26 04:39:00 +0500

    - Macros are synchronized with Chromium

--- source/base/macros.h
+++ source/base/macros.h
@@ -8,26 +8,21 @@
 #ifndef _ASPIA_BASE__MACROS_H
 #define _ASPIA_BASE__MACROS_H
 
+// Put this in the declarations for a class to be uncopyable.
+#define DISALLOW_COPY(TypeName) \
+  TypeName(const TypeName&) = delete
+
 // Put this in the declarations for a class to be unassignable.
-#define DISALLOW_ASSIGN(TypeName) \
-  void operator=(const TypeName&) = delete
+#define DISALLOW_ASSIGN(TypeName) TypeName& operator=(const TypeName&) = delete
 
-//
-// A macro to disallow the copy constructor and operator= functions.
-// This should be used in the private: declarations for a class.
-//
-#define DISALLOW_COPY_AND_ASSIGN(TypeName)   \
-    TypeName(const TypeName&) = delete;      \
-    void operator=(const TypeName&) = delete
+// Put this in the declarations for a class to be uncopyable and unassignable.
+#define DISALLOW_COPY_AND_ASSIGN(TypeName) \
+  DISALLOW_COPY(TypeName);                 \
+  DISALLOW_ASSIGN(TypeName)
 
-//
-// A macro to disallow all the implicit constructors, namely the default
-// constructor, copy constructor and operator= functions.
-//
-// This should be used in the declarations for a class that wants to prevent
-// anyone from instantiating it. This is especially useful for classes
-// containing only static methods.
-//
+// A macro to disallow all the implicit constructors, namely the
+// default constructor, copy constructor and operator= functions.
+// This is especially useful for classes containing only static methods.
 #define DISALLOW_IMPLICIT_CONSTRUCTORS(TypeName) \
   TypeName() = delete;                           \
   DISALLOW_COPY_AND_ASSIGN(TypeName)
