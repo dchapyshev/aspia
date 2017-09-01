04c69eebcb50ff276cb3370cf2129c46a9960773
Dmitry Chapyshev <dmitry@aspia.ru>
2017-09-02 00:49:43 +0500

    - Implement RouteEnumerator and ShareEnumerator classes.

---
+++ source/network/route_enumerator.cc
@@ -0,0 +1,84 @@
+//
+// PROJECT:         Aspia Remote Desktop
+// FILE:            network/route_enumerator.cc
+// LICENSE:         Mozilla Public License Version 2.0
+// PROGRAMMERS:     Dmitry Chapyshev (dmitry@aspia.ru)
+//
+
+#include "network/route_enumerator.h"
+
+#include <winsock2.h>
+
+namespace aspia {
+
+RouteEnumerator::RouteEnumerator()
+{
+    ULONG buffer_size = sizeof(MIB_IPFORWARDTABLE);
+
+    forward_table_buffer_ = std::make_unique<uint8_t[]>(buffer_size);
+    forward_table_ = reinterpret_cast<PMIB_IPFORWARDTABLE>(forward_table_buffer_.get());
+
+    DWORD error_code = GetIpForwardTable(forward_table_, &buffer_size, 0);
+    if (error_code != NO_ERROR)
+    {
+        if (error_code == ERROR_INSUFFICIENT_BUFFER)
+        {
+            forward_table_buffer_ = std::make_unique<uint8_t[]>(buffer_size);
+            forward_table_ = reinterpret_cast<PMIB_IPFORWARDTABLE>(forward_table_buffer_.get());
+
+            error_code = GetIpForwardTable(forward_table_, &buffer_size, 0);
+            if (error_code == NO_ERROR)
+                return;
+        }
+
+        forward_table_buffer_.reset();
+        forward_table_ = nullptr;
+    }
+}
+
+bool RouteEnumerator::IsAtEnd() const
+{
+    if (!forward_table_)
+        return true;
+
+    return index_ >= forward_table_->dwNumEntries;
+}
+
+void RouteEnumerator::Advance()
+{
+    ++index_;
+}
+
+static std::string IpToString(DWORD ip)
+{
+    in_addr ip_address;
+    ip_address.S_un.S_addr = ip;
+
+    char* buffer = inet_ntoa(ip_address);
+    if (!buffer)
+        return std::string();
+
+    return buffer;
+}
+
+std::string RouteEnumerator::GetDestonation() const
+{
+    return IpToString(forward_table_->table[index_].dwForwardDest);
+}
+
+std::string RouteEnumerator::GetMask() const
+{
+    return IpToString(forward_table_->table[index_].dwForwardMask);
+}
+
+std::string RouteEnumerator::GetGateway() const
+{
+    return IpToString(forward_table_->table[index_].dwForwardNextHop);
+}
+
+uint32_t RouteEnumerator::GetMetric() const
+{
+    return forward_table_->table[index_].dwForwardMetric1;
+}
+
+} // namespace aspia
---
+++ source/network/share_enumerator.h
@@ -0,0 +1,55 @@
+//
+// PROJECT:         Aspia Remote Desktop
+// FILE:            network/share_enumerator.h
+// LICENSE:         Mozilla Public License Version 2.0
+// PROGRAMMERS:     Dmitry Chapyshev (dmitry@aspia.ru)
+//
+
+#ifndef _ASPIA_NETWORK__SHARE_ENUMERATOR_H
+#define _ASPIA_NETWORK__SHARE_ENUMERATOR_H
+
+#include "base/macros.h"
+
+#include <lm.h>
+#include <string>
+
+namespace aspia {
+
+class ShareEnumerator
+{
+public:
+    ShareEnumerator();
+    ~ShareEnumerator();
+
+    bool IsAtEnd() const;
+    void Advance();
+
+    std::string GetName() const;
+
+    enum class Type
+    {
+        UNKNOWN   = 0,
+        DISK      = 1,
+        PRINTER   = 2,
+        DEVICE    = 3,
+        IPC       = 4,
+        SPECIAL   = 5,
+        TEMPORARY = 6
+    };
+
+    Type GetType() const;
+    std::string GetDescription() const;
+    uint32_t GetCurrentUses() const;
+    uint32_t GetMaximumUses() const;
+
+private:
+    PSHARE_INFO_502 share_info_ = nullptr;
+    DWORD total_entries_ = 0;
+    DWORD current_entry_ = 0;
+
+    DISALLOW_COPY_AND_ASSIGN(ShareEnumerator);
+};
+
+} // namespace aspia
+
+#endif // _ASPIA_NETWORK__SHARE_ENUMERATOR_H
