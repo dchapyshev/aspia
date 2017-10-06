c149efde4d2ed6bdf9f9e66ea32403ccd19dc239
Dmitry Chapyshev <dmitry@aspia.ru>
2017-10-06 18:16:34 +0500

    - Added proto files for system information session.

--- source/proto/build.cmd
+++ source/proto/build.cmd
@@ -9,8 +9,8 @@ protoc auth_session.proto --cpp_out=.
 protoc desktop_session.proto --cpp_out=.
 protoc power_session.proto --cpp_out=.
 protoc file_transfer_session.proto --cpp_out=.
+protoc system_info_session.proto --cpp_out=.
 
 protoc host_user.proto --cpp_out=.
 
 protoc key_exchange.proto --cpp_out=.
-pause
