70cf36d3c2aa6a684c22fad3e00ee96ce68e3eef
Dmitry Chapyshev <dmitry@aspia.ru>
2017-10-10 17:26:13 +0500

    - Move categories to parent groups.

--- source/ui/system_info/category_group_hardware.cc
+++ source/ui/system_info/category_group_hardware.cc
@@ -7,13 +7,314 @@
 
 #include "protocol/system_info_constants.h"
 #include "ui/system_info/category_group_hardware.h"
-#include "ui/system_info/category_group_dmi.h"
-#include "ui/system_info/category_group_storage.h"
-#include "ui/system_info/category_group_display.h"
-#include "ui/system_info/category_group_windows_devices.h"
 #include "ui/system_info/category_info.h"
 
 namespace aspia {
+
+class CategoryDmi : public CategoryInfo
+{
+public:
+    CategoryDmi(const char* guid, UINT name_id, UINT icon_id);
+    virtual ~CategoryDmi() = default;
+};
+
+CategoryDmi::CategoryDmi(const char* guid, UINT name_id, UINT icon_id)
+    : CategoryInfo(guid, name_id, icon_id)
+{
+    ColumnList* column_list = mutable_column_list();
+    column_list->emplace_back(IDS_SI_COLUMN_PARAMETER, 200);
+    column_list->emplace_back(IDS_SI_COLUMN_VALUE, 200);
+}
+
+class CategoryDmiBios : public CategoryDmi
+{
+public:
+    CategoryDmiBios()
+        : CategoryDmi(system_info::hardware::dmi::kBIOS,
+                      IDS_SI_CATEGORY_DMI_BIOS,
+                      IDI_BIOS)
+    {
+        // Nothing
+    }
+
+    ~CategoryDmiBios() = default;
+
+    void Parse(const std::string& data, Output* output) final
+    {
+        // TODO
+    }
+
+private:
+    DISALLOW_COPY_AND_ASSIGN(CategoryDmiBios);
+};
+
+class CategoryDmiSystem : public CategoryDmi
+{
+public:
+    CategoryDmiSystem()
+        : CategoryDmi(system_info::hardware::dmi::kSystem,
+                      IDS_SI_CATEGORY_DMI_SYSTEM,
+                      IDI_COMPUTER)
+    {
+        // Nothing
+    }
+
+    ~CategoryDmiSystem() = default;
+
+    void Parse(const std::string& data, Output* output) final
+    {
+        // TODO
+    }
+
+private:
+    DISALLOW_COPY_AND_ASSIGN(CategoryDmiSystem);
+};
+
+class CategoryDmiMotherboard : public CategoryDmi
+{
+public:
+    CategoryDmiMotherboard()
+        : CategoryDmi(system_info::hardware::dmi::kMotherboard,
+                      IDS_SI_CATEGORY_DMI_MOTHERBOARD,
+                      IDI_MOTHERBOARD)
+    {
+        // Nothing
+    }
+
+    ~CategoryDmiMotherboard() = default;
+
+    void Parse(const std::string& data, Output* output) final
+    {
+        // TODO
+    }
+
+private:
+    DISALLOW_COPY_AND_ASSIGN(CategoryDmiMotherboard);
+};
+
+class CategoryDmiChassis : public CategoryDmi
+{
+public:
+    CategoryDmiChassis()
+        : CategoryDmi(system_info::hardware::dmi::kChassis,
+                      IDS_SI_CATEGORY_DMI_CHASSIS,
+                      IDI_SERVER)
+    {
+        // Nothing
+    }
+
+    ~CategoryDmiChassis() = default;
+
+    void Parse(const std::string& data, Output* output) final
+    {
+        // TODO
+    }
+
+private:
+    DISALLOW_COPY_AND_ASSIGN(CategoryDmiChassis);
+};
+
+class CategoryDmiCaches : public CategoryDmi
+{
+public:
+    CategoryDmiCaches()
+        : CategoryDmi(system_info::hardware::dmi::kCaches,
+                      IDS_SI_CATEGORY_DMI_CACHES,
+                      IDI_CHIP)
+    {
+        // Nothing
+    }
+
+    ~CategoryDmiCaches() = default;
+
+    void Parse(const std::string& data, Output* output) final
+    {
+        // TODO
+    }
+
+private:
+    DISALLOW_COPY_AND_ASSIGN(CategoryDmiCaches);
+};
+
+class CategoryDmiProcessors : public CategoryDmi
+{
+public:
+    CategoryDmiProcessors()
+        : CategoryDmi(system_info::hardware::dmi::kProcessors,
+                      IDS_SI_CATEGORY_DMI_PROCESSORS,
+                      IDI_PROCESSOR)
+    {
+        // Nothing
+    }
+
+    ~CategoryDmiProcessors() = default;
+
+    void Parse(const std::string& data, Output* output) final
+    {
+        // TODO
+    }
+
+private:
+    DISALLOW_COPY_AND_ASSIGN(CategoryDmiProcessors);
+};
+
+class CategoryDmiMemoryDevices : public CategoryDmi
+{
+public:
+    CategoryDmiMemoryDevices()
+        : CategoryDmi(system_info::hardware::dmi::kMemoryDevices,
+                      IDS_SI_CATEGORY_DMI_MEMORY_DEVICES,
+                      IDI_MEMORY)
+    {
+        // Nothing
+    }
+
+    ~CategoryDmiMemoryDevices() = default;
+
+    void Parse(const std::string& data, Output* output) final
+    {
+        // TODO
+    }
+
+private:
+    DISALLOW_COPY_AND_ASSIGN(CategoryDmiMemoryDevices);
+};
+
+class CategoryDmiSystemSlots : public CategoryDmi
+{
+public:
+    CategoryDmiSystemSlots()
+        : CategoryDmi(system_info::hardware::dmi::kSystemSlots,
+                      IDS_SI_CATEGORY_DMI_SYSTEM_SLOTS,
+                      IDI_PORT)
+    {
+        // Nothing
+    }
+
+    ~CategoryDmiSystemSlots() = default;
+
+    void Parse(const std::string& data, Output* output) final
+    {
+        // TODO
+    }
+
+private:
+    DISALLOW_COPY_AND_ASSIGN(CategoryDmiSystemSlots);
+};
+
+class CategoryDmiPortConnectors : public CategoryDmi
+{
+public:
+    CategoryDmiPortConnectors()
+        : CategoryDmi(system_info::hardware::dmi::kPortConnectors,
+                      IDS_SI_CATEGORY_DMI_PORT_CONNECTORS,
+                      IDI_PORT)
+    {
+        // Nothing
+    }
+
+    ~CategoryDmiPortConnectors() = default;
+
+    void Parse(const std::string& data, Output* output) final
+    {
+        // TODO
+    }
+
+private:
+    DISALLOW_COPY_AND_ASSIGN(CategoryDmiPortConnectors);
+};
+
+class CategoryDmiOnboardDevices : public CategoryDmi
+{
+public:
+    CategoryDmiOnboardDevices()
+        : CategoryDmi(system_info::hardware::dmi::kOnboardDevices,
+                      IDS_SI_CATEGORY_DMI_ONBOARD_DEVICES,
+                      IDI_MOTHERBOARD)
+    {
+        // Nothing
+    }
+
+    ~CategoryDmiOnboardDevices() = default;
+
+    void Parse(const std::string& data, Output* output) final
+    {
+        // TODO
+    }
+
+private:
+    DISALLOW_COPY_AND_ASSIGN(CategoryDmiOnboardDevices);
+};
+
+class CategoryDmiBuildinPointing : public CategoryDmi
+{
+public:
+    CategoryDmiBuildinPointing()
+        : CategoryDmi(system_info::hardware::dmi::kBuildinPointing,
+                      IDS_SI_CATEGORY_DMI_BUILDIN_POINTING,
+                      IDI_MOUSE)
+    {
+        // Nothing
+    }
+
+    ~CategoryDmiBuildinPointing() = default;
+
+    void Parse(const std::string& data, Output* output) final
+    {
+        // TODO
+    }
+
+private:
+    DISALLOW_COPY_AND_ASSIGN(CategoryDmiBuildinPointing);
+};
+
+class CategoryDmiPortableBattery : public CategoryDmi
+{
+public:
+    CategoryDmiPortableBattery()
+        : CategoryDmi(system_info::hardware::dmi::kPortableBattery,
+                      IDS_SI_CATEGORY_DMI_PORTABLE_BATTERY,
+                      IDI_BATTERY)
+    {
+        // Nothing
+    }
+
+    ~CategoryDmiPortableBattery() = default;
+
+    void Parse(const std::string& data, Output* output) final
+    {
+        // TODO
+    }
+
+private:
+    DISALLOW_COPY_AND_ASSIGN(CategoryDmiPortableBattery);
+};
+
+class CategoryGroupDMI : public CategoryGroup
+{
+public:
+    CategoryGroupDMI()
+        : CategoryGroup(IDS_SI_CATEGORY_DMI, IDI_COMPUTER)
+    {
+        CategoryList* child_list = mutable_child_list();
+
+        child_list->emplace_back(std::make_unique<CategoryDmiBios>());
+        child_list->emplace_back(std::make_unique<CategoryDmiSystem>());
+        child_list->emplace_back(std::make_unique<CategoryDmiMotherboard>());
+        child_list->emplace_back(std::make_unique<CategoryDmiChassis>());
+        child_list->emplace_back(std::make_unique<CategoryDmiCaches>());
+        child_list->emplace_back(std::make_unique<CategoryDmiProcessors>());
+        child_list->emplace_back(std::make_unique<CategoryDmiMemoryDevices>());
+        child_list->emplace_back(std::make_unique<CategoryDmiSystemSlots>());
+        child_list->emplace_back(std::make_unique<CategoryDmiPortConnectors>());
+        child_list->emplace_back(std::make_unique<CategoryDmiOnboardDevices>());
+        child_list->emplace_back(std::make_unique<CategoryDmiBuildinPointing>());
+        child_list->emplace_back(std::make_unique<CategoryDmiPortableBattery>());
+    }
+
+private:
+    DISALLOW_COPY_AND_ASSIGN(CategoryGroupDMI);
+};
 
 class CategoryCPU : public CategoryInfo
 {
@@ -37,6 +338,182 @@ public:
 
 private:
     DISALLOW_COPY_AND_ASSIGN(CategoryCPU);
+};
+
+class CategoryOpticalDrives : public CategoryInfo
+{
+public:
+    CategoryOpticalDrives()
+        : CategoryInfo(system_info::hardware::storage::kOpticalDrives,
+                       IDS_SI_CATEGORY_STORAGE_OPTICAL_DRIVES,
+                       IDI_DRIVE_DISK)
+    {
+        ColumnList* column_list = mutable_column_list();
+        column_list->emplace_back(IDS_SI_COLUMN_PARAMETER, 200);
+        column_list->emplace_back(IDS_SI_COLUMN_VALUE, 200);
+    }
+
+    ~CategoryOpticalDrives() = default;
+
+    void Parse(const std::string& data, Output* output) final
+    {
+        // TODO
+    }
+
+private:
+    DISALLOW_COPY_AND_ASSIGN(CategoryOpticalDrives);
+};
+
+class CategoryATA : public CategoryInfo
+{
+public:
+    CategoryATA()
+        : CategoryInfo(system_info::hardware::storage::kATA,
+                       IDS_SI_CATEGORY_STORAGE_ATA,
+                       IDI_DRIVE)
+    {
+        ColumnList* column_list = mutable_column_list();
+        column_list->emplace_back(IDS_SI_COLUMN_PARAMETER, 200);
+        column_list->emplace_back(IDS_SI_COLUMN_VALUE, 200);
+    }
+
+    ~CategoryATA() = default;
+
+    void Parse(const std::string& data, Output* output) final
+    {
+        // TODO
+    }
+
+private:
+    DISALLOW_COPY_AND_ASSIGN(CategoryATA);
+};
+
+class CategorySMART : public CategoryInfo
+{
+public:
+    CategorySMART()
+        : CategoryInfo(system_info::hardware::storage::kSMART,
+                       IDS_SI_CATEGORY_STORAGE_SMART,
+                       IDI_DRIVE)
+    {
+        ColumnList* column_list = mutable_column_list();
+        column_list->emplace_back(IDS_SI_COLUMN_PARAMETER, 200);
+        column_list->emplace_back(IDS_SI_COLUMN_VALUE, 200);
+    }
+
+    ~CategorySMART() = default;
+
+    void Parse(const std::string& data, Output* output) final
+    {
+        // TODO
+    }
+
+private:
+    DISALLOW_COPY_AND_ASSIGN(CategorySMART);
+};
+
+class CategoryGroupStorage : public CategoryGroup
+{
+public:
+    CategoryGroupStorage()
+        : CategoryGroup(IDS_SI_CATEGORY_STORAGE, IDI_DRIVE)
+    {
+        CategoryList* child_list = mutable_child_list();
+        child_list->emplace_back(std::make_unique<CategoryOpticalDrives>());
+        child_list->emplace_back(std::make_unique<CategoryATA>());
+        child_list->emplace_back(std::make_unique<CategorySMART>());
+    }
+
+private:
+    DISALLOW_COPY_AND_ASSIGN(CategoryGroupStorage);
+};
+
+class CategoryWindowsVideo : public CategoryInfo
+{
+public:
+    CategoryWindowsVideo()
+        : CategoryInfo(system_info::hardware::display::kWindowsVideo,
+                       IDS_SI_CATEGORY_DISPLAY_WINDOWS_VIDEO,
+                       IDI_MONITOR)
+    {
+        ColumnList* column_list = mutable_column_list();
+        column_list->emplace_back(IDS_SI_COLUMN_PARAMETER, 200);
+        column_list->emplace_back(IDS_SI_COLUMN_VALUE, 200);
+    }
+
+    ~CategoryWindowsVideo() = default;
+
+    void Parse(const std::string& data, Output* output) final
+    {
+        // TODO
+    }
+
+private:
+    DISALLOW_COPY_AND_ASSIGN(CategoryWindowsVideo);
+};
+
+class CategoryMonitor : public CategoryInfo
+{
+public:
+    CategoryMonitor()
+        : CategoryInfo(system_info::hardware::display::kMonitor,
+                       IDS_SI_CATEGORY_DISPLAY_MONITOR,
+                       IDI_MONITOR)
+    {
+        ColumnList* column_list = mutable_column_list();
+        column_list->emplace_back(IDS_SI_COLUMN_PARAMETER, 200);
+        column_list->emplace_back(IDS_SI_COLUMN_VALUE, 200);
+    }
+
+    ~CategoryMonitor() = default;
+
+    void Parse(const std::string& data, Output* output) final
+    {
+        // TODO
+    }
+
+private:
+    DISALLOW_COPY_AND_ASSIGN(CategoryMonitor);
+};
+
+class CategoryOpenGL : public CategoryInfo
+{
+public:
+    CategoryOpenGL()
+        : CategoryInfo(system_info::hardware::display::kOpenGL,
+                       IDS_SI_CATEGORY_DISPLAY_OPENGL,
+                       IDI_CLAPPERBOARD)
+    {
+        ColumnList* column_list = mutable_column_list();
+        column_list->emplace_back(IDS_SI_COLUMN_PARAMETER, 200);
+        column_list->emplace_back(IDS_SI_COLUMN_VALUE, 200);
+    }
+
+    ~CategoryOpenGL() = default;
+
+    void Parse(const std::string& data, Output* output) final
+    {
+        // TODO
+    }
+
+private:
+    DISALLOW_COPY_AND_ASSIGN(CategoryOpenGL);
+};
+
+class CategoryGroupDisplay : public CategoryGroup
+{
+public:
+    CategoryGroupDisplay()
+        : CategoryGroup(IDS_SI_CATEGORY_DISPLAY, IDI_MONITOR)
+    {
+        CategoryList* child_list = mutable_child_list();
+        child_list->emplace_back(std::make_unique<CategoryWindowsVideo>());
+        child_list->emplace_back(std::make_unique<CategoryMonitor>());
+        child_list->emplace_back(std::make_unique<CategoryOpenGL>());
+    }
+
+private:
+    DISALLOW_COPY_AND_ASSIGN(CategoryGroupDisplay);
 };
 
 class CategoryPrinters : public CategoryInfo
@@ -85,6 +562,69 @@ public:
 
 private:
     DISALLOW_COPY_AND_ASSIGN(CategoryPowerOptions);
+};
+
+class CategoryAllDevices : public CategoryInfo
+{
+public:
+    CategoryAllDevices()
+        : CategoryInfo(system_info::hardware::windows_devices::kAll,
+                       IDS_SI_CATEGORY_WINDOWS_DEVICES_ALL,
+                       IDI_PCI)
+    {
+        ColumnList* column_list = mutable_column_list();
+        column_list->emplace_back(IDS_SI_COLUMN_PARAMETER, 200);
+        column_list->emplace_back(IDS_SI_COLUMN_VALUE, 200);
+    }
+
+    ~CategoryAllDevices() = default;
+
+    void Parse(const std::string& data, Output* output) final
+    {
+        // TODO
+    }
+
+private:
+    DISALLOW_COPY_AND_ASSIGN(CategoryAllDevices);
+};
+
+class CategoryUnknownDevices : public CategoryInfo
+{
+public:
+    CategoryUnknownDevices()
+        : CategoryInfo(system_info::hardware::windows_devices::kUnknown,
+                       IDS_SI_CATEGORY_WINDOWS_DEVICES_UNK,
+                       IDI_PCI)
+    {
+        ColumnList* column_list = mutable_column_list();
+        column_list->emplace_back(IDS_SI_COLUMN_PARAMETER, 200);
+        column_list->emplace_back(IDS_SI_COLUMN_VALUE, 200);
+    }
+
+    ~CategoryUnknownDevices() = default;
+
+    void Parse(const std::string& data, Output* output) final
+    {
+        // TODO
+    }
+
+private:
+    DISALLOW_COPY_AND_ASSIGN(CategoryUnknownDevices);
+};
+
+class CategoryGroupWindowDevices : public CategoryGroup
+{
+public:
+    CategoryGroupWindowDevices()
+        : CategoryGroup(IDS_SI_CATEGORY_WINDOWS_DEVICES, IDI_PCI)
+    {
+        CategoryList* child_list = mutable_child_list();
+        child_list->emplace_back(std::make_unique<CategoryAllDevices>());
+        child_list->emplace_back(std::make_unique<CategoryUnknownDevices>());
+    }
+
+private:
+    DISALLOW_COPY_AND_ASSIGN(CategoryGroupWindowDevices);
 };
 
 CategoryGroupHardware::CategoryGroupHardware()
--- source/ui/system_info/category_group_os.cc
+++ source/ui/system_info/category_group_os.cc
@@ -7,8 +7,6 @@
 
 #include "protocol/system_info_constants.h"
 #include "ui/system_info/category_group_os.h"
-#include "ui/system_info/category_group_users.h"
-#include "ui/system_info/category_group_event_log.h"
 #include "ui/system_info/category_info.h"
 
 namespace aspia {
@@ -83,6 +81,182 @@ public:
 
 private:
     DISALLOW_COPY_AND_ASSIGN(CategoryEnvironmentVariables);
+};
+
+class CategoryApplications : public CategoryInfo
+{
+public:
+    CategoryApplications()
+        : CategoryInfo(system_info::operating_system::event_logs::kApplications,
+                       IDS_SI_CATEGORY_EVENT_LOG_APPLICATIONS,
+                       IDI_ERROR_LOG)
+    {
+        ColumnList* column_list = mutable_column_list();
+        column_list->emplace_back(IDS_SI_COLUMN_PARAMETER, 200);
+        column_list->emplace_back(IDS_SI_COLUMN_VALUE, 200);
+    }
+
+    ~CategoryApplications() = default;
+
+    void Parse(const std::string& data, Output* output) final
+    {
+        // TODO
+    }
+
+private:
+    DISALLOW_COPY_AND_ASSIGN(CategoryApplications);
+};
+
+class CategorySecurity : public CategoryInfo
+{
+public:
+    CategorySecurity()
+        : CategoryInfo(system_info::operating_system::event_logs::kSecurity,
+                       IDS_SI_CATEGORY_EVENT_LOG_SECURITY,
+                       IDI_ERROR_LOG)
+    {
+        ColumnList* column_list = mutable_column_list();
+        column_list->emplace_back(IDS_SI_COLUMN_PARAMETER, 200);
+        column_list->emplace_back(IDS_SI_COLUMN_VALUE, 200);
+    }
+
+    ~CategorySecurity() = default;
+
+    void Parse(const std::string& data, Output* output) final
+    {
+        // TODO
+    }
+
+private:
+    DISALLOW_COPY_AND_ASSIGN(CategorySecurity);
+};
+
+class CategorySystem : public CategoryInfo
+{
+public:
+    CategorySystem()
+        : CategoryInfo(system_info::operating_system::event_logs::kSystem,
+                       IDS_SI_CATEGORY_EVENT_LOG_SYSTEM,
+                       IDI_ERROR_LOG)
+    {
+        ColumnList* column_list = mutable_column_list();
+        column_list->emplace_back(IDS_SI_COLUMN_PARAMETER, 200);
+        column_list->emplace_back(IDS_SI_COLUMN_VALUE, 200);
+    }
+
+    ~CategorySystem() = default;
+
+    void Parse(const std::string& data, Output* output) final
+    {
+        // TODO
+    }
+
+private:
+    DISALLOW_COPY_AND_ASSIGN(CategorySystem);
+};
+
+class CategoryGroupEventLog : public CategoryGroup
+{
+public:
+    CategoryGroupEventLog()
+        : CategoryGroup(IDS_SI_CATEGORY_EVENT_LOGS, IDI_BOOKS_STACK)
+    {
+        CategoryList* child_list = mutable_child_list();
+        child_list->emplace_back(std::make_unique<CategoryApplications>());
+        child_list->emplace_back(std::make_unique<CategorySecurity>());
+        child_list->emplace_back(std::make_unique<CategorySystem>());
+    }
+
+private:
+    DISALLOW_COPY_AND_ASSIGN(CategoryGroupEventLog);
+};
+
+class CategoryUsers : public CategoryInfo
+{
+public:
+    CategoryUsers()
+        : CategoryInfo(system_info::operating_system::users_and_groups::kUsers,
+                       IDS_SI_CATEGORY_USERS,
+                       IDI_USER)
+    {
+        ColumnList* column_list = mutable_column_list();
+        column_list->emplace_back(IDS_SI_COLUMN_PARAMETER, 200);
+        column_list->emplace_back(IDS_SI_COLUMN_VALUE, 200);
+    }
+
+    ~CategoryUsers() = default;
+
+    void Parse(const std::string& data, Output* output) final
+    {
+        // TODO
+    }
+
+private:
+    DISALLOW_COPY_AND_ASSIGN(CategoryUsers);
+};
+
+class CategoryUserGroups : public CategoryInfo
+{
+public:
+    CategoryUserGroups()
+        : CategoryInfo(system_info::operating_system::users_and_groups::kUserGroups,
+                       IDS_SI_CATEGORY_USER_GROUPS,
+                       IDI_USERS)
+    {
+        ColumnList* column_list = mutable_column_list();
+        column_list->emplace_back(IDS_SI_COLUMN_PARAMETER, 200);
+        column_list->emplace_back(IDS_SI_COLUMN_VALUE, 200);
+    }
+
+    ~CategoryUserGroups() = default;
+
+    void Parse(const std::string& data, Output* output) final
+    {
+        // TODO
+    }
+
+private:
+    DISALLOW_COPY_AND_ASSIGN(CategoryUserGroups);
+};
+
+class CategoryActiveSessions : public CategoryInfo
+{
+public:
+    CategoryActiveSessions()
+        : CategoryInfo(system_info::operating_system::users_and_groups::kActiveSessions,
+                       IDS_SI_CATEGORY_ACTIVE_SESSIONS,
+                       IDI_USERS)
+    {
+        ColumnList* column_list = mutable_column_list();
+        column_list->emplace_back(IDS_SI_COLUMN_PARAMETER, 200);
+        column_list->emplace_back(IDS_SI_COLUMN_VALUE, 200);
+    }
+
+    ~CategoryActiveSessions() = default;
+
+    void Parse(const std::string& data, Output* output) final
+    {
+        // TODO
+    }
+
+private:
+    DISALLOW_COPY_AND_ASSIGN(CategoryActiveSessions);
+};
+
+class CategoryGroupUsers : public CategoryGroup
+{
+public:
+    CategoryGroupUsers()
+        : CategoryGroup(IDS_SI_CATEGORY_USERS_AND_GROUPS, IDI_USERS)
+    {
+        CategoryList* child_list = mutable_child_list();
+        child_list->emplace_back(std::make_unique<CategoryUsers>());
+        child_list->emplace_back(std::make_unique<CategoryUserGroups>());
+        child_list->emplace_back(std::make_unique<CategoryActiveSessions>());
+    }
+
+private:
+    DISALLOW_COPY_AND_ASSIGN(CategoryGroupUsers);
 };
 
 CategoryGroupOS::CategoryGroupOS()
