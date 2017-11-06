5f735a05882bece438c48869dc428140022bbd50
Dmitry Chapyshev <dmitry@aspia.ru>
2017-11-07 02:12:15 +0500

    - Implemented ProcessEnumerator class.

---
+++ source/base/process/process_enumerator.cc
@@ -0,0 +1,206 @@
+//
+// PROJECT:         Aspia Remote Desktop
+// FILE:            base/process/process_enumerator.cc
+// LICENSE:         Mozilla Public License Version 2.0
+// PROGRAMMERS:     Dmitry Chapyshev (dmitry@aspia.ru)
+//
+
+#include "base/process/process_enumerator.h"
+#include "base/strings/string_util.h"
+#include "base/strings/unicode.h"
+#include "base/logging.h"
+
+#include <tlhelp32.h>
+#include <psapi.h>
+#include <memory>
+
+namespace aspia {
+
+ProcessEnumerator::ProcessEnumerator()
+{
+    snapshot_.Reset(CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0));
+    if (!snapshot_.IsValid())
+    {
+        LOG(WARNING) << "CreateToolhelp32Snapshot() failed: " << GetLastSystemErrorString();
+        return;
+    }
+
+    memset(&process_entry_, 0, sizeof(process_entry_));
+    process_entry_.dwSize = sizeof(process_entry_);
+    if (!Process32FirstW(snapshot_, &process_entry_))
+    {
+        LOG(WARNING) << "Process32FirstW() failed: " << GetLastSystemErrorString();
+        snapshot_.Reset();
+    }
+
+    for (;;)
+    {
+        current_process_.Reset(OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ,
+                                           FALSE,
+                                           process_entry_.th32ProcessID));
+
+        if (!current_process_.IsValid())
+        {
+            LOG(WARNING) << "OpenProcess() failed: " << GetLastSystemErrorString();
+
+            if (!Process32NextW(snapshot_, &process_entry_))
+            {
+                DWORD error_code = GetLastError();
+                if (error_code != ERROR_NO_MORE_FILES)
+                {
+                    LOG(WARNING) << "Process32NextW() failed: " << GetLastSystemErrorString();
+                }
+
+                snapshot_.Reset();
+                return;
+            }
+        }
+        else
+        {
+            break;
+        }
+    }
+}
+
+bool ProcessEnumerator::IsAtEnd() const
+{
+    return !snapshot_.IsValid();
+}
+
+void ProcessEnumerator::Advance()
+{
+    for (;;)
+    {
+        if (!Process32NextW(snapshot_, &process_entry_))
+        {
+            DWORD error_code = GetLastError();
+            if (error_code != ERROR_NO_MORE_FILES)
+            {
+                LOG(WARNING) << "Process32NextW() failed: " << GetLastSystemErrorString();
+            }
+
+            snapshot_.Reset();
+            return;
+        }
+
+        current_process_.Reset(OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ,
+                                           FALSE,
+                                           process_entry_.th32ProcessID));
+        if (!current_process_.IsValid())
+        {
+            LOG(WARNING) << "OpenProcess() failed: " << GetLastSystemErrorString();
+            continue;
+        }
+
+        break;
+    }
+}
+
+std::string ProcessEnumerator::GetProcessName() const
+{
+    return UTF8fromUNICODE(process_entry_.szExeFile);
+}
+
+std::string ProcessEnumerator::GetFilePath() const
+{
+    WCHAR file_path[MAX_PATH];
+
+    if (!GetModuleFileNameExW(current_process_.Get(), nullptr, file_path, _countof(file_path)))
+    {
+        LOG(WARNING) << "GetModuleFileNameExW() failed: " << GetLastSystemErrorString();
+        return std::string();
+    }
+
+    return UTF8fromUNICODE(file_path);
+}
+
+std::string ProcessEnumerator::GetFileDescription() const
+{
+    WCHAR file_path[MAX_PATH];
+
+    if (!GetModuleFileNameExW(current_process_.Get(), nullptr, file_path, _countof(file_path)))
+    {
+        LOG(WARNING) << "GetModuleFileNameExW() failed: " << GetLastSystemErrorString();
+        return std::string();
+    }
+
+    DWORD handle = 0;
+    DWORD size = GetFileVersionInfoSizeW(file_path, &handle);;
+    if (!size)
+    {
+        LOG(WARNING) << "GetFileVersionInfoSizeW() failed: " << GetLastSystemErrorString();
+        return std::string();
+    }
+
+    std::unique_ptr<uint8_t[]> buffer = std::make_unique<uint8_t[]>(size);
+
+    if (!GetFileVersionInfoW(file_path, handle, size, buffer.get()))
+    {
+        LOG(WARNING) << "GetFileVersionInfoW() failed: " << GetLastSystemErrorString();
+        return std::string();
+    }
+
+    struct LangAndCodepage
+    {
+        WORD language;
+        WORD code_page;
+    } *translate;
+
+    UINT value_size;
+
+    if (!VerQueryValueW(buffer.get(),
+                        L"\\VarFileInfo\\Translation",
+                        reinterpret_cast<LPVOID*>(&translate),
+                        &value_size))
+    {
+        LOG(WARNING) << "VerQueryValueW() failed: " << GetLastSystemErrorString();
+        return std::string();
+    }
+
+    std::wstring subblock = StringPrintfW(L"\\StringFileInfo\\%04x%04x\\FileDescription",
+                                          translate->language,
+                                          translate->code_page);
+
+    WCHAR* description = nullptr;
+
+    if (!VerQueryValueW(buffer.get(),
+                        subblock.c_str(),
+                        reinterpret_cast<LPVOID*>(&description),
+                        &value_size))
+    {
+        LOG(WARNING) << "VerQueryValueW() failed: " << GetLastSystemErrorString();
+        return std::string();
+    }
+
+    return UTF8fromUNICODE(description);
+}
+
+uint64_t ProcessEnumerator::GetUsedMemory() const
+{
+    PROCESS_MEMORY_COUNTERS memory_counters;
+    memset(&memory_counters, 0, sizeof(memory_counters));
+
+    if (!GetProcessMemoryInfo(current_process_.Get(), &memory_counters, sizeof(memory_counters)))
+    {
+        LOG(WARNING) << "GetProcessMemoryInfo() failed: " << GetLastSystemErrorString();
+        return 0;
+    }
+
+    return memory_counters.WorkingSetSize;
+}
+
+uint64_t ProcessEnumerator::GetUsedSwap() const
+{
+    PROCESS_MEMORY_COUNTERS memory_counters;
+    memset(&memory_counters, 0, sizeof(memory_counters));
+
+    if (!GetProcessMemoryInfo(current_process_.Get(), &memory_counters, sizeof(memory_counters)))
+    {
+        LOG(WARNING) << "GetProcessMemoryInfo() failed: " << GetLastSystemErrorString();
+        return 0;
+    }
+
+    return memory_counters.PagefileUsage;
+}
+
+} // namespace aspia
---
+++ source/base/process/process_enumerator.h
@@ -0,0 +1,42 @@
+//
+// PROJECT:         Aspia Remote Desktop
+// FILE:            base/process/process_enumerator.h
+// LICENSE:         Mozilla Public License Version 2.0
+// PROGRAMMERS:     Dmitry Chapyshev (dmitry@aspia.ru)
+//
+
+#ifndef _ASPIA_BASE__PROCESS_PROCESS_ENUMERATOR_H
+#define _ASPIA_BASE__PROCESS_PROCESS_ENUMERATOR_H
+
+#include "base/scoped_object.h"
+
+#include <tlhelp32.h>
+#include <string>
+
+namespace aspia {
+
+class ProcessEnumerator
+{
+public:
+    ProcessEnumerator();
+
+    bool IsAtEnd() const;
+    void Advance();
+
+    std::string GetProcessName() const;
+    std::string GetFilePath() const;
+    std::string GetFileDescription() const;
+    uint64_t GetUsedMemory() const;
+    uint64_t GetUsedSwap() const;
+
+private:
+    ScopedHandle snapshot_;
+    ScopedHandle current_process_;
+    PROCESSENTRY32W process_entry_;
+
+    DISALLOW_COPY_AND_ASSIGN(ProcessEnumerator);
+};
+
+} // namespace aspia
+
+#endif // _ASPIA_BASE__PROCESS_PROCESS_ENUMERATOR_H
