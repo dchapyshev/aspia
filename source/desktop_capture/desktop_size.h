1071647afa7f5b6b367f5d95aaecbff5f10055df
Dmitry Chapyshev <dmitry@aspia.ru>
2017-11-28 17:22:43 +0500

    - Make DesktopSize class header-only.

--- source/desktop_capture/desktop_size.h
+++ source/desktop_capture/desktop_size.h
@@ -16,19 +16,53 @@ class DesktopSize
 {
 public:
     DesktopSize() = default;
-    DesktopSize(int32_t width, int32_t height);
-    DesktopSize(const DesktopSize& other);
+
+    DesktopSize(int32_t width, int32_t height)
+        : width_(width),
+          height_(height)
+    {
+        // Nothing
+    }
+
+    DesktopSize(const DesktopSize& other)
+        : width_(other.width_),
+          height_(other.height_)
+    {
+        // Nothing
+    }
+
     ~DesktopSize() = default;
 
-    int32_t Width() const;
-    int32_t Height() const;
+    int32_t Width() const { return width_; }
+    int32_t Height() const { return height_; }
 
-    void Set(int32_t width, int32_t height);
-    bool IsEmpty() const;
-    bool IsEqual(const DesktopSize& other) const;
-    void Clear();
+    void Set(int32_t width, int32_t height)
+    {
+        width_ = width;
+        height_ = height;
+    }
 
-    DesktopSize& operator=(const DesktopSize& other);
+    bool IsEmpty() const
+    {
+        return width_ <= 0 || height_ <= 0;
+    }
+
+    bool IsEqual(const DesktopSize& other) const
+    {
+        return width_ == other.width_ && height_ == other.height_;
+    }
+
+    void Clear()
+    {
+        width_ = 0;
+        height_ = 0;
+    }
+
+    DesktopSize& operator=(const DesktopSize& other)
+    {
+        Set(other.width_, other.height_);
+        return *this;
+    }
 
 private:
     int32_t width_ = 0;
