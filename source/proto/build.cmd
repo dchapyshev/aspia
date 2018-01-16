642701c652cdd3d3119029732ab1d4f5bb1dc0df
Dmitry Chapyshev <dmitry@aspia.ru>
2018-01-16 16:15:14 +0500

    - Added build scrypt for categories.

--- source/proto/build.cmd
+++ source/proto/build.cmd
@@ -1,16 +1,3 @@
-protoc status.proto --cpp_out=.
-
-protoc auth_session_message.proto --cpp_out=.
-protoc desktop_session_message.proto --cpp_out=.
-protoc power_session_message.proto --cpp_out=.
-protoc file_transfer_session_message.proto --cpp_out=.
-
-protoc auth_session.proto --cpp_out=.
-protoc desktop_session.proto --cpp_out=.
-protoc power_session.proto --cpp_out=.
-protoc file_transfer_session.proto --cpp_out=.
-protoc system_info_session.proto --cpp_out=.
-
-protoc host_user.proto --cpp_out=.
-
-protoc key_exchange.proto --cpp_out=.
+for %%f in (*.proto) do (
+	protoc %%f --cpp_out=.
+)
