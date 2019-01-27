dcc0d68f2ccc013c9d61f2b7fbbaecb91f054a6e
Dmitry Chapyshev <dmitry@aspia.ru>
2019-01-28 01:35:55 +0500

    - Update Russian translation.

--- source/common/translations/aspia_common_ru.ts
+++ source/common/translations/aspia_common_ru.ts
@@ -102,77 +102,77 @@
     <message>
         <location filename="../ui/about_dialog.cc" line="88"/>
         <source>Version: %1</source>
-        <translation type="unfinished">Версия: %1</translation>
+        <translation>Версия: %1</translation>
     </message>
     <message>
         <location filename="../ui/about_dialog.cc" line="92"/>
         <source>Aspia is free software released under GNU General Public License 3.</source>
-        <translation type="unfinished">Aspia - свободное программное обеспечение, распространяющееся по лицензии GNU General Public License 3.</translation>
+        <translation>Aspia - свободное программное обеспечение, распространяющееся по лицензии GNU General Public License 3.</translation>
     </message>
     <message>
         <location filename="../ui/about_dialog.cc" line="93"/>
         <source>You can get a copy of license here:</source>
-        <translation type="unfinished">Вы можете получить копию лицензии по адресу:</translation>
+        <translation>Вы можете получить копию лицензии по адресу:</translation>
     </message>
     <message>
         <location filename="../ui/about_dialog.cc" line="98"/>
         <source>You can also get a translation of GNU GPL license here:</source>
-        <translation type="unfinished">Ознакомиться с переводом лицензии на ваш язык можно по адресу:</translation>
+        <translation>Ознакомиться с переводом лицензии на ваш язык можно по адресу:</translation>
     </message>
     <message>
         <location filename="../ui/about_dialog.cc" line="103"/>
         <source>Links:</source>
-        <translation type="unfinished">Ссылки:</translation>
+        <translation>Ссылки:</translation>
     </message>
     <message>
         <location filename="../ui/about_dialog.cc" line="104"/>
         <source>Home page:</source>
-        <translation type="unfinished">Домашняя страница:</translation>
+        <translation>Домашняя страница:</translation>
     </message>
     <message>
         <location filename="../ui/about_dialog.cc" line="105"/>
         <source>GitHub page:</source>
-        <translation type="unfinished">GitHub страница:</translation>
+        <translation>GitHub страница:</translation>
     </message>
     <message>
         <location filename="../ui/about_dialog.cc" line="108"/>
         <source>Developers:</source>
-        <translation type="unfinished">Разработчики:</translation>
+        <translation>Разработчики:</translation>
     </message>
     <message>
         <location filename="../ui/about_dialog.cc" line="110"/>
         <source>Translators:</source>
-        <translation type="unfinished">Переводчики:</translation>
+        <translation>Переводчики:</translation>
     </message>
     <message>
         <location filename="../ui/about_dialog.cc" line="112"/>
         <source>Third-party components:</source>
-        <translation type="unfinished">Сторонние компоненты:</translation>
+        <translation>Сторонние компоненты:</translation>
     </message>
     <message>
         <location filename="../ui/about_dialog.cc" line="129"/>
         <source>Path: %1</source>
-        <translation type="unfinished">Путь: %1</translation>
+        <translation>Путь: %1</translation>
     </message>
     <message>
         <location filename="../ui/about_dialog.cc" line="130"/>
         <source>Compilation date: %1</source>
-        <translation type="unfinished">Дата компиляции: %1</translation>
+        <translation>Дата компиляции: %1</translation>
     </message>
     <message>
         <location filename="../ui/about_dialog.cc" line="131"/>
         <source>Compilation time: %1</source>
-        <translation type="unfinished">Время компиляции: %1</translation>
+        <translation>Время компиляции: %1</translation>
     </message>
     <message>
         <location filename="../ui/about_dialog.cc" line="135"/>
         <source>%1 version: %2</source>
-        <translation type="unfinished">Версия %1: %2</translation>
+        <translation>Версия %1: %2</translation>
     </message>
     <message>
         <location filename="../ui/about_dialog.cc" line="155"/>
         <source>https://aspia.org/en/donate.html</source>
-        <translation type="unfinished">https://aspia.org/ru/donate.html</translation>
+        <translation>https://aspia.org/ru/donate.html</translation>
     </message>
 </context>
 </TS>
