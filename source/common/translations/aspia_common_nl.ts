73efbbea728c1e7930d79033c392ad1091708f01
Mark Jansen <mark.jansen@reactos.org>
2019-02-03 14:06:06 +0100

    aspia_common_nl Dutch translation

--- source/common/translations/aspia_common_nl.ts
+++ source/common/translations/aspia_common_nl.ts
@@ -11,17 +11,17 @@
     <message>
         <location filename="../ui/about_dialog.ui" line="64"/>
         <source>General</source>
-        <translation type="unfinished">Basis</translation>
+        <translation>Basis</translation>
     </message>
     <message>
         <location filename="../ui/about_dialog.ui" line="91"/>
         <source>License</source>
-        <translation type="unfinished"></translation>
+        <translation>Licentie</translation>
     </message>
     <message>
         <location filename="../ui/about_dialog.ui" line="108"/>
         <source>Service Information</source>
-        <translation type="unfinished"></translation>
+        <translation>Service Informatie</translation>
     </message>
     <message>
         <location filename="../ui/about_dialog.ui" line="123"/>
@@ -82,72 +82,72 @@
     <message>
         <location filename="../ui/about_dialog.cc" line="88"/>
         <source>Version: %1</source>
-        <translation type="unfinished">Versie: %1</translation>
+        <translation>Versie: %1</translation>
     </message>
     <message>
         <location filename="../ui/about_dialog.cc" line="92"/>
         <source>Aspia is free software released under GNU General Public License 3.</source>
-        <translation type="unfinished">Aspia is gratis software verspreid onder de GNU General Public License 3.</translation>
+        <translation>Aspia is gratis software verspreid onder de GNU General Public License 3.</translation>
     </message>
     <message>
         <location filename="../ui/about_dialog.cc" line="93"/>
         <source>You can get a copy of license here:</source>
-        <translation type="unfinished">Download een kopie van de licentie:</translation>
+        <translation>Download een kopie van de licentie:</translation>
     </message>
     <message>
         <location filename="../ui/about_dialog.cc" line="98"/>
         <source>You can also get a translation of GNU GPL license here:</source>
-        <translation type="unfinished">Download hier een vertaling van de GNU GPL:</translation>
+        <translation>Download hier een vertaling van de GNU GPL:</translation>
     </message>
     <message>
         <location filename="../ui/about_dialog.cc" line="103"/>
         <source>Links:</source>
-        <translation type="unfinished">Links:</translation>
+        <translation>Links:</translation>
     </message>
     <message>
         <location filename="../ui/about_dialog.cc" line="104"/>
         <source>Home page:</source>
-        <translation type="unfinished">Website:</translation>
+        <translation>Website:</translation>
     </message>
     <message>
         <location filename="../ui/about_dialog.cc" line="105"/>
         <source>GitHub page:</source>
-        <translation type="unfinished">GitHub pagina:</translation>
+        <translation>GitHub pagina:</translation>
     </message>
     <message>
         <location filename="../ui/about_dialog.cc" line="108"/>
         <source>Developers:</source>
-        <translation type="unfinished">Ontwikkelaars:</translation>
+        <translation>Ontwikkelaars:</translation>
     </message>
     <message>
         <location filename="../ui/about_dialog.cc" line="110"/>
         <source>Translators:</source>
-        <translation type="unfinished">Vertalingen:</translation>
+        <translation>Vertalingen:</translation>
     </message>
     <message>
         <location filename="../ui/about_dialog.cc" line="112"/>
         <source>Third-party components:</source>
-        <translation type="unfinished">Third-party componenten:</translation>
+        <translation>Third-party componenten:</translation>
     </message>
     <message>
         <location filename="../ui/about_dialog.cc" line="129"/>
         <source>Path: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Pad: %1</translation>
     </message>
     <message>
         <location filename="../ui/about_dialog.cc" line="130"/>
         <source>Compilation date: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Compilatie datum: %1</translation>
     </message>
     <message>
         <location filename="../ui/about_dialog.cc" line="131"/>
         <source>Compilation time: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Compilatie tijd: %1</translation>
     </message>
     <message>
         <location filename="../ui/about_dialog.cc" line="135"/>
         <source>%1 version: %2</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 versie: %2</translation>
     </message>
 </context>
 </TS>
