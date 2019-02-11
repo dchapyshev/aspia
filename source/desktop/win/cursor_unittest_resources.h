fb17bad4d8171ff297a6221894cc3f92da09a1b3
Dmitry Chapyshev <dmitry@aspia.ru>
2019-02-12 02:17:07 +0500

    - Added tests (based on WebRTC source code).

---
+++ source/desktop/win/cursor_unittest_resources.h
@@ -0,0 +1,32 @@
+//
+// Aspia Project
+// Copyright (C) 2019 Dmitry Chapyshev <dmitry@aspia.ru>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+//
+
+#ifndef DESKTOP__WIN__CURSOR_UNITTEST_RESOURCES_H
+#define DESKTOP__WIN__CURSOR_UNITTEST_RESOURCES_H
+
+#define IDD_CURSOR1_24BPP 101
+#define IDD_CURSOR1_32BPP 102
+#define IDD_CURSOR1_8BPP 103
+
+#define IDD_CURSOR2_1BPP 104
+#define IDD_CURSOR2_32BPP 105
+
+#define IDD_CURSOR3_4BPP 106
+#define IDD_CURSOR3_32BPP 107
+
+#endif // DESKTOP__WIN__CURSOR_UNITTEST_RESOURCES_H
