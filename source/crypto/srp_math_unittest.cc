18730b1357010167e9df88de8438329b7d32cf86
Dmitry Chapyshev <dmitry@aspia.ru>
2019-03-02 20:17:44 +0500

    - Fix build.

--- source/crypto/srp_math_unittest.cc
+++ source/crypto/srp_math_unittest.cc
@@ -18,8 +18,6 @@
 
 #include <gtest/gtest.h>
 
-#include <QString>
-
 #include "crypto/srp_constants.h"
 #include "crypto/srp_math.h"
 
@@ -27,8 +25,8 @@ namespace crypto {
 
 TEST(srp_math_test, test_vector)
 {
-    QString I = "alice";
-    QString p = "password123";
+    std::string I = "alice";
+    std::string p = "password123";
 
     const uint8_t a_buf[] = { 0x60, 0x97, 0x55, 0x27, 0x03, 0x5C, 0xF2, 0xAD, 0x19, 0x89, 0x80,
                               0x6F, 0x04, 0x07, 0x21, 0x0B, 0xC8, 0x1E, 0xDC, 0x04, 0xE2, 0x76,
@@ -163,14 +161,14 @@ TEST(srp_math_test, test_vector)
     ASSERT_EQ(u_string.size(), sizeof(u_ref_buf));
     ASSERT_EQ(memcmp(u_string.c_str(), u_ref_buf, sizeof(u_ref_buf)), 0);
 
-    BigNum x = SrpMath::calc_x(s, I.toUtf8(), p.toUtf8());
+    BigNum x = SrpMath::calc_x(s, I, p);
     ASSERT_TRUE(x.isValid());
 
     std::string x_string = x.toStdString();
     ASSERT_EQ(x_string.size(), sizeof(x_ref_buf));
     ASSERT_EQ(memcmp(x_string.c_str(), x_ref_buf, sizeof(x_ref_buf)), 0);
 
-    BigNum v = SrpMath::calc_v(I.toUtf8(), p.toUtf8(), s, N, g);
+    BigNum v = SrpMath::calc_v(I, p, s, N, g);
     ASSERT_TRUE(v.isValid());
 
     std::string v_string = v.toStdString();
