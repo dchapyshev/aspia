f423af452fe3efaa765cea53180726c77a501396
Dmitry Chapyshev <dmitry@aspia.ru>
2019-03-24 14:47:15 +0500

    - Save the address book only if it has been changed. Fixes tab pinning for read-only address books.

--- source/console/console_main_window.cc
+++ source/console/console_main_window.cc
@@ -742,8 +742,11 @@ void MainWindow::onTabContextMenu(const QPoint& pos)
     {
         if (pin_action->isChecked())
         {
-            if (!tab->save())
-                return;
+            if (tab->isChanged())
+            {
+                if (!tab->save())
+                    return;
+            }
 
             ui.tab_widget->setTabIcon(
                 tab_index, QIcon(QStringLiteral(":/img/address-book-pinned.png")));
