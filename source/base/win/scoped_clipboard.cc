bc2102ff63b558650f1408e2517c4babea806f62
Dmitry Chapyshev <dmitry@aspia.ru>
2020-02-03 23:47:00 +0500

    - Added ScopedClipboard and ScopedHGLOBAL implementation.

---
+++ source/base/win/scoped_clipboard.cc
@@ -0,0 +1,106 @@
+//
+// Aspia Project
+// Copyright (C) 2020 Dmitry Chapyshev <dmitry@aspia.ru>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+//
+
+#include "base/win/scoped_clipboard.h"
+
+#include "base/logging.h"
+
+namespace base::win {
+
+ScopedClipboard::~ScopedClipboard()
+{
+    if (opened_)
+    {
+        //
+        // CloseClipboard() must be called with anonymous access token. See
+        // crbug.com/441834.
+        //
+        BOOL result = ImpersonateAnonymousToken(GetCurrentThread());
+        CHECK(result);
+
+        CloseClipboard();
+
+        result = RevertToSelf();
+        CHECK(result);
+    }
+}
+
+bool ScopedClipboard::init(HWND owner)
+{
+    const int kMaxAttemptsToOpenClipboard = 5;
+    const DWORD kSleepTimeBetweenAttempts = 5;
+
+    if (opened_)
+    {
+        DLOG(LS_ERROR) << "Attempt to open an already opened clipboard";
+        return true;
+    }
+
+    // This code runs on the UI thread, so we can block only very briefly.
+    for (int attempt = 0; attempt < kMaxAttemptsToOpenClipboard; ++attempt)
+    {
+        if (attempt > 0)
+        {
+            Sleep(kSleepTimeBetweenAttempts);
+        }
+
+        if (OpenClipboard(owner))
+        {
+            opened_ = true;
+            return true;
+        }
+    }
+
+    return false;
+}
+
+BOOL ScopedClipboard::empty()
+{
+    if (!opened_)
+    {
+        DLOG(LS_WARNING) << "Clipboard is not open";
+        return FALSE;
+    }
+
+    return EmptyClipboard();
+}
+
+void ScopedClipboard::setData(UINT format, HANDLE mem)
+{
+    if (!opened_)
+    {
+        DLOG(LS_WARNING) << "Clipboard is not open";
+        return;
+    }
+
+    // The caller must not close the handle that SetClipboardData returns.
+    SetClipboardData(format, mem);
+}
+
+HANDLE ScopedClipboard::data(UINT format) const
+{
+    if (!opened_)
+    {
+        DLOG(LS_WARNING) << "Clipboard is not open";
+        return nullptr;
+    }
+
+    return GetClipboardData(format);
+}
+
+} // namespace base::win
---
+++ source/base/win/scoped_hglobal.h
@@ -0,0 +1,74 @@
+//
+// Aspia Project
+// Copyright (C) 2020 Dmitry Chapyshev <dmitry@aspia.ru>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+//
+
+#ifndef BASE__WIN__SCOPED_HGLOBAL_H
+#define BASE__WIN__SCOPED_HGLOBAL_H
+
+#include "base/macros_magic.h"
+
+#include <Windows.h>
+
+namespace base::win {
+
+template<class T>
+class ScopedHGLOBAL
+{
+public:
+    explicit ScopedHGLOBAL(HGLOBAL glob) : glob_(glob)
+    {
+        data_ = static_cast<T*>(GlobalLock(glob_));
+    }
+
+    ~ScopedHGLOBAL()
+    {
+        GlobalUnlock(glob_);
+    }
+
+    T* get()
+    {
+        return data_;
+    }
+
+    size_t size() const
+    {
+        return GlobalSize(glob_);
+    }
+
+    T* operator->() const
+    {
+        assert(data_ != 0);
+        return data_;
+    }
+
+    T* release()
+    {
+        T* data = data_;
+        data_ = nullptr;
+        return data;
+    }
+
+private:
+    HGLOBAL glob_;
+    T* data_;
+
+    DISALLOW_COPY_AND_ASSIGN(ScopedHGLOBAL);
+};
+
+} // namespace base::win
+
+#endif // BASE__WIN__SCOPED_HGLOBAL_H
