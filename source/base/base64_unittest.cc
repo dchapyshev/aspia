dae2914fd56492c273287b9747bb8f0cb09fc490
Dmitry Chapyshev <dmitry@aspia.ru>
2020-02-26 00:46:06 +0500

    - Fix build.

--- source/base/base64_unittest.cc
+++ source/base/base64_unittest.cc
@@ -20,8 +20,6 @@
 
 #include <gtest/gtest.h>
 
-#include <QByteArray>
-
 namespace base {
 
 namespace {
@@ -78,41 +76,23 @@ TEST(base64_test, empty)
     EXPECT_TRUE(decoded.empty());
 }
 
-TEST(base64_test, DISABLED_benchmark_qt)
-{
-    for (int i = 0; i < kIterationCount; ++i)
-    {
-        QByteArray source_1(kTestData1);
-        QByteArray encoded_1 = source_1.toBase64();
-        QByteArray decoded_1 = QByteArray::fromBase64(encoded_1);
-
-        EXPECT_EQ(source_1, decoded_1);
-
-        QByteArray source_2(kTestData1);
-        QByteArray encoded_2 = source_2.toBase64();
-        QByteArray decoded_2 = QByteArray::fromBase64(encoded_2);
-
-        EXPECT_EQ(source_2, decoded_2);
-    }
-}
-
 TEST(base64_test, DISABLED_benchmark)
 {
     for (int i = 0; i < kIterationCount; ++i)
     {
-        QByteArray source_1(kTestData1);
-        QByteArray encoded_1 = Base64::encodeT<QByteArray, QByteArray>(source_1);
+        std::string source_1(kTestData1);
+        std::string encoded_1 = Base64::encode(source_1);
 
-        QByteArray decoded_1;
-        Base64::decodeT<QByteArray, QByteArray>(encoded_1, &decoded_1);
+        std::string decoded_1;
+        Base64::decode(encoded_1, &decoded_1);
 
         EXPECT_EQ(source_1, decoded_1);
 
-        QByteArray source_2(kTestData2);
-        QByteArray encoded_2 = Base64::encodeT<QByteArray, QByteArray>(source_2);
+        std::string source_2(kTestData2);
+        std::string encoded_2 = Base64::encode(source_2);
 
-        QByteArray decoded_2;
-        Base64::decodeT<QByteArray, QByteArray>(encoded_2, &decoded_2);
+        std::string decoded_2;
+        Base64::decode(encoded_2, &decoded_2);
 
         EXPECT_EQ(source_2, decoded_2);
     }
