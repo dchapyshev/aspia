c883243fddddae2e1beedab35e96da0c346426a6
Dmitry Chapyshev <dmitry@aspia.ru>
2020-04-27 17:43:41 +0500

    Re-add ScaleReducer class.

---
+++ source/codec/scale_reducer.cc
@@ -0,0 +1,129 @@
+//
+// Aspia Project
+// Copyright (C) 2020 Dmitry Chapyshev <dmitry@aspia.ru>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+//
+
+#include "codec/scale_reducer.h"
+
+#include "base/logging.h"
+#include "desktop/frame_simple.h"
+
+#include <libyuv/scale_argb.h>
+
+namespace codec {
+
+namespace {
+
+const int kMinScaleFactor = 30;
+const int kMaxScaleFactor = 100;
+const int kDefScaleFactor = 100;
+
+int div(int num, int div)
+{
+    return (num + div - 1) / div;
+}
+
+desktop::Size scaledSize(const desktop::Size& source_size, int scale_factor)
+{
+    return desktop::Size(div(source_size.width() * scale_factor, kDefScaleFactor),
+                         div(source_size.height() * scale_factor, kDefScaleFactor));
+}
+
+desktop::Rect scaledRect(const desktop::Rect& source_rect, int scale_factor)
+{
+    int left = (source_rect.left() * scale_factor) / kDefScaleFactor;
+    int top = (source_rect.top() * scale_factor) / kDefScaleFactor;
+    int right = div(source_rect.right() * scale_factor, kDefScaleFactor);
+    int bottom = div(source_rect.bottom() * scale_factor, kDefScaleFactor);
+
+    return desktop::Rect::makeLTRB(left, top, right, bottom);
+}
+
+} // namespace
+
+ScaleReducer::ScaleReducer(int scale_factor)
+    : scale_factor_(scale_factor)
+{
+    // Nothing
+}
+
+// static
+ScaleReducer* ScaleReducer::create(int scale_factor)
+{
+    if (scale_factor < kMinScaleFactor || scale_factor > kMaxScaleFactor)
+        return nullptr;
+
+    return new ScaleReducer(scale_factor);
+}
+
+const desktop::Frame* ScaleReducer::scaleFrame(const desktop::Frame* source_frame)
+{
+    DCHECK(source_frame);
+    DCHECK(!source_frame->constUpdatedRegion().isEmpty());
+    DCHECK(source_frame->format() == desktop::PixelFormat::ARGB());
+
+    if (scale_factor_ == kDefScaleFactor)
+        return source_frame;
+
+    if (last_frame_size_ != source_frame->size())
+        scaled_frame_.reset();
+
+    if (!scaled_frame_)
+    {
+        desktop::Size size = scaledSize(source_frame->size(), scale_factor_);
+
+        scaled_frame_ = desktop::FrameSimple::create(size, source_frame->format());
+        if (!scaled_frame_)
+            return nullptr;
+    }
+
+    const desktop::Size& source_size = source_frame->size();
+    const desktop::Size& scaled_size = scaled_frame_->size();
+
+    desktop::Rect scaled_frame_rect = desktop::Rect::makeSize(scaled_size);
+    desktop::Region* updated_region = scaled_frame_->updatedRegion();
+
+    updated_region->clear();
+
+    for (desktop::Region::Iterator it(source_frame->constUpdatedRegion()); !it.isAtEnd(); it.advance())
+    {
+        desktop::Rect scaled_rect = scaledRect(it.rect(), scale_factor_);
+        scaled_rect.intersectWith(scaled_frame_rect);
+
+        if (libyuv::ARGBScaleClip(source_frame->frameData(),
+                                  source_frame->stride(),
+                                  source_size.width(),
+                                  source_size.height(),
+                                  scaled_frame_->frameData(),
+                                  scaled_frame_->stride(),
+                                  scaled_size.width(),
+                                  scaled_size.height(),
+                                  scaled_rect.x(),
+                                  scaled_rect.y(),
+                                  scaled_rect.width(),
+                                  scaled_rect.height(),
+                                  libyuv::kFilterBox) == -1)
+        {
+            LOG(LS_WARNING) << "libyuv::ARGBScaleClip failed";
+        }
+
+        updated_region->addRect(scaled_rect);
+    }
+
+    return scaled_frame_.get();
+}
+
+} // namespace codec
---
+++ source/codec/scale_reducer.h
@@ -0,0 +1,55 @@
+//
+// Aspia Project
+// Copyright (C) 2020 Dmitry Chapyshev <dmitry@aspia.ru>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+//
+
+#ifndef CODEC__SCALE_REDUCER_H
+#define CODEC__SCALE_REDUCER_H
+
+#include "base/macros_magic.h"
+#include "desktop/geometry.h"
+
+#include <memory>
+
+namespace desktop {
+class Frame;
+} // namespace aspia
+
+namespace codec {
+
+class ScaleReducer
+{
+public:
+    ~ScaleReducer() = default;
+
+    static ScaleReducer* create(int scale_factor);
+
+    const desktop::Frame* scaleFrame(const desktop::Frame* source_frame);
+
+protected:
+    explicit ScaleReducer(int scale_factor);
+
+private:
+    const int scale_factor_;
+    std::unique_ptr<desktop::Frame> scaled_frame_;
+    desktop::Size last_frame_size_;
+
+    DISALLOW_COPY_AND_ASSIGN(ScaleReducer);
+};
+
+} // namespace codec
+
+#endif // CODEC__SCALE_REDUCER_H
