0b68444b1f7a438b44f1231dac943c2e985ffde9
Dmitry Chapyshev <dmitry@aspia.ru>
2020-09-21 00:34:29 +0500

    Fix warnings.

--- source/client/ui/desktop_panel.cc
+++ source/client/ui/desktop_panel.cc
@@ -234,7 +234,7 @@ bool DesktopPanel::sendKeyCombinations() const
     return ui.action_send_key_combinations->isChecked();
 }
 
-void DesktopPanel::enterEvent(QEvent* event)
+void DesktopPanel::enterEvent(QEvent* /* event */)
 {
     leaved_ = false;
 
@@ -250,7 +250,7 @@ void DesktopPanel::enterEvent(QEvent* event)
     }
 }
 
-void DesktopPanel::leaveEvent(QEvent* event)
+void DesktopPanel::leaveEvent(QEvent* /* event */)
 {
     leaved_ = true;
 
