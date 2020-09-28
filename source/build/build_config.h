5ee2fbee065c8e3210edb34a969184a3f2e2e8b7
Dmitry Chapyshev <dmitry@aspia.ru>
2020-09-29 00:53:18 +0500

    Add ARM/ARM64 cpu detection.

--- source/build/build_config.h
+++ source/build/build_config.h
@@ -45,15 +45,33 @@
 
 // Architecture detection.
 #if defined(_M_X64) || defined(__x86_64__)
+
 #define ARCH_CPU_X86_FAMILY    1
 #define ARCH_CPU_X86_64        1
 #define ARCH_CPU_64_BITS       1
 #define ARCH_CPU_LITTLE_ENDIAN 1
+
 #elif defined(_M_IX86) || defined(__i386__)
+
 #define ARCH_CPU_X86_FAMILY    1
 #define ARCH_CPU_X86           1
 #define ARCH_CPU_32_BITS       1
 #define ARCH_CPU_LITTLE_ENDIAN 1
+
+#elif defined(__ARMEL__)
+
+#define ARCH_CPU_ARM_FAMILY    1
+#define ARCH_CPU_ARMEL         1
+#define ARCH_CPU_32_BITS       1
+#define ARCH_CPU_LITTLE_ENDIAN 1
+
+#elif defined(__aarch64__) || defined(_M_ARM64)
+
+#define ARCH_CPU_ARM_FAMILY    1
+#define ARCH_CPU_ARM64         1
+#define ARCH_CPU_64_BITS       1
+#define ARCH_CPU_LITTLE_ENDIAN 1
+
 #else
 #error Unknown architecture
 #endif
