7d4d0843d977780428311fad3bbc34087f66bf15
Dmitry Chapyshev <dmitry@aspia.ru>
2020-11-23 00:12:59 +0500

    Added tests for Frame class.

---
+++ source/base/desktop/frame_unittest.cc
@@ -0,0 +1,71 @@
+//
+// Aspia Project
+// Copyright (C) 2020 Dmitry Chapyshev <dmitry@aspia.ru>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+//
+
+#include "base/desktop/frame_simple.h"
+
+#include <gtest/gtest.h>
+
+namespace base {
+
+namespace {
+
+std::unique_ptr<Frame> createTestFrame(Rect rect, int pixels_value)
+{
+    Size size = rect.size();
+    auto frame = FrameSimple::create(size);
+    frame->setTopLeft(rect.topLeft());
+    memset(frame->frameData(), pixels_value, frame->stride() * size.height());
+    return frame;
+}
+
+} // namespace
+
+TEST(FrameTest, Performance)
+{
+    Rect frame_rect = Rect::makeWH(1024, 768);
+    auto frame1 = createTestFrame(frame_rect, 0);
+    auto frame2 = createTestFrame(frame_rect, 0xff);
+
+    struct
+    {
+        Rect src_rect;
+        Rect dst_rect;
+    } cases[] =
+    {
+        { Rect::makeXYWH(0, 0, 120, 175), Rect::makeXYWH(50, 50, 120, 175) },
+        { Rect::makeXYWH(100, 200, 50, 100), Rect::makeXYWH(500, 400, 50, 100) },
+        { Rect::makeXYWH(75, 60, 10, 5), Rect::makeXYWH(30, 45, 10, 5) },
+        { Rect::makeXYWH(500, 200, 200, 200), Rect::makeXYWH(200, 500, 200, 200) },
+        { Rect::makeXYWH(350, 0, 100, 50), Rect::makeXYWH(0, 350, 100, 50) }
+    };
+
+    for (size_t n = 0; n < 100000; ++n)
+    {
+        for (size_t i = 0; i < std::size(cases); ++i)
+        {
+            frame1->copyPixelsFrom(*frame2, cases[i].src_rect.topLeft(), cases[i].dst_rect);
+        }
+
+        for (size_t i = 0; i < std::size(cases); ++i)
+        {
+            frame2->copyPixelsFrom(*frame1, cases[i].src_rect.topLeft(), cases[i].dst_rect);
+        }
+    }
+}
+
+} // namespace base
