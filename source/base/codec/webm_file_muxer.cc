bcd55ab4aef9559d983326910289100717aeaa55
Dmitry Chapyshev <dmitry@aspia.ru>
2021-09-12 22:32:19 +0500

    Enable WebmFileWriter/WebmFileMuxer classes.

--- source/base/codec/webm_file_muxer.cc
+++ source/base/codec/webm_file_muxer.cc
@@ -169,7 +169,7 @@ bool WebmFileMuxer::writeFrame(std::string_view frame,
                                uint64_t track_num, bool is_key)
 {
     if (!segment_->AddFrame(reinterpret_cast<const uint8_t*>(frame.data()), frame.size(),
-                            track_num, timestamp.count(), is_key))
+                            track_num, static_cast<uint64_t>(timestamp.count()), is_key))
     {
         LOG(LS_ERROR) << "AddFrame failed";
         return false;
