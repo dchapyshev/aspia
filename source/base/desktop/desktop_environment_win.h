fadf52846f27e9b6e58110adddc8de0c78b089df
Dmitry Chapyshev <dmitry@aspia.ru>
2022-02-21 17:54:13 +0500

    Updating per user system parameters.

--- source/base/desktop/desktop_environment_win.h
+++ source/base/desktop/desktop_environment_win.h
@@ -29,7 +29,7 @@ public:
     DesktopEnvironmentWin();
     ~DesktopEnvironmentWin() override;
 
-    static void prepareEnvironment();
+    static void updateEnvironment();
 
 protected:
     void disableWallpaper() override;
