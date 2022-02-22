bfb1b3655a03c092ae4a87b9ce9d5179572541a5
Dmitry Chapyshev <dmitry@aspia.ru>
2022-02-22 23:34:44 +0500

    Added local_shared_ptr implementation (forked from Boost).

---
+++ source/base/memory/local_memory.h
@@ -0,0 +1,19 @@
+#ifndef BASE_MEMORY_LOCAL_MEMORY_H
+#define BASE_MEMORY_LOCAL_MEMORY_H
+
+//
+//  smart_ptr.hpp
+//
+// Distributed under the Boost Software License, Version 1.0. (See
+// accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+//  See http://www.boost.org/libs/smart_ptr/weak_ptr.htm for documentation.
+//
+
+#include "base/memory/local_memory_impl/local_shared_ptr.h"
+#include "base/memory/local_memory_impl/local_weak_ptr.h"
+#include "base/memory/local_memory_impl/make_local_shared.h"
+#include "base/memory/local_memory_impl/enable_shared_from_this.h"
+
+#endif // BASE_MEMORY_LOCAL_MEMORY_H
