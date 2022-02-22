2e6eb181bab4edb2924bdd8f1cc50ff15c70d65a
Dmitry Chapyshev <dmitry@aspia.ru>
2022-02-22 23:37:39 +0500

    Update file header.

--- source/base/memory/local_memory.h
+++ source/base/memory/local_memory.h
@@ -1,15 +1,23 @@
+//
+// Aspia Project
+// Copyright (C) 2016-2022 Dmitry Chapyshev <dmitry@aspia.ru>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+//
+
 #ifndef BASE_MEMORY_LOCAL_MEMORY_H
 #define BASE_MEMORY_LOCAL_MEMORY_H
-
-//
-//  smart_ptr.hpp
-//
-// Distributed under the Boost Software License, Version 1.0. (See
-// accompanying file LICENSE_1_0.txt or copy at
-// http://www.boost.org/LICENSE_1_0.txt)
-//
-//  See http://www.boost.org/libs/smart_ptr/weak_ptr.htm for documentation.
-//
 
 #include "base/memory/local_memory_impl/local_shared_ptr.h"
 #include "base/memory/local_memory_impl/local_weak_ptr.h"
