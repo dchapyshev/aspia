d5986bf9bccf84e582b65a6e75f74fa72eb18ec5
Dmitry Chapyshev <dmitry@dmitry-ubuntu.localdomain>
2023-08-28 01:33:06 +0500

    Implement FilePathWatcher for linux (based on Chromium source code).

--- source/base/files/file_path_watcher.h
+++ source/base/files/file_path_watcher.h
@@ -62,13 +62,13 @@ public:
         // properly while the object is still alive.
         virtual void cancel() = 0;
 
-    protected:
-        friend class FilePathWatcher;
-
         std::shared_ptr<TaskRunner> taskRunner() const
         {
             return task_runner_;
         }
+
+    protected:
+        friend class FilePathWatcher;
 
         // Must be called before the PlatformDelegate is deleted.
         void setCancelled()
