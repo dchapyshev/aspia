2b44d695250191b480e752da8d8301b17c804015
Dmitry Chapyshev <dmitry@aspia.ru>
2026-06-21 23:09:49 +0500

    Refactoring.

---
+++ source/host/ui/sources.cmake
@@ -0,0 +1,51 @@
+#
+# Aspia Project
+# Copyright (C) 2016-2026 Dmitry Chapyshev <dmitry@aspia.ru>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+#
+
+collect_sources(SOURCE_HOST_CORE_UI
+    application.cc
+    application.h
+    change_password_dialog.cc
+    change_password_dialog.h
+    change_password_dialog.ui
+    check_password_dialog.cc
+    check_password_dialog.h
+    check_password_dialog.ui
+    config_dialog.cc
+    config_dialog.h
+    config_dialog.ui
+    connect_confirm_dialog.cc
+    connect_confirm_dialog.h
+    connect_confirm_dialog.ui
+    host_main.cc
+    host_main.h
+    host_window.cc
+    host_window.h
+    host_window.ui
+    notifier_window.cc
+    notifier_window.h
+    notifier_window.ui
+    security_log_dialog.cc
+    security_log_dialog.h
+    security_log_dialog.ui
+    settings_util.cc
+    settings_util.h
+    user_dialog.cc
+    user_dialog.h
+    user_dialog.ui
+    user_settings.cc
+    user_settings.h)
