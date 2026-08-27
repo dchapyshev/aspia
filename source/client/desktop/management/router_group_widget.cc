fc679c20e5f35b72d2556b7864c8763ed96cc6e5
Dmitry Chapyshev <dmitry@aspia.ru>
2026-08-27 11:11:57 +0500

    Fix default page count.

--- source/client/desktop/management/router_group_widget.cc
+++ source/client/desktop/management/router_group_widget.cc
@@ -104,6 +104,7 @@ RouterGroupWidget::RouterGroupWidget(QWidget* parent)
     ui->combo_hosts_page_size->addItem("50", QVariant::fromValue<qint64>(50));
     ui->combo_hosts_page_size->addItem("100", QVariant::fromValue<qint64>(100));
     ui->combo_hosts_page_size->setCurrentIndex(2);
+    hosts_page_.setPageSize(ui->combo_hosts_page_size->currentData().toLongLong());
 
     ui->button_hosts_next->setIconOnRight(true);
 
